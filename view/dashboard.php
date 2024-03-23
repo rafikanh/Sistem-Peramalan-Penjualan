4d282d8192b13f94696baa9328de9ad60d802e50
Rafika Nurhayati <89893773+rafikanh@users.noreply.github.com>
2024-03-23 15:21:04 +0700

    update-dashboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -15,7 +15,7 @@
 
 <body>
     <?php include '../view/component/sidebar.php'; ?>
-    <div class="content-large">
+    <div class="content-medium">
         <div class="container-fluid">
             <h1>Hai, Admin!</h1>
             <p>Selamat datang di website Sistem Peramalan Penjualan</p>
@@ -23,145 +23,77 @@
             <?php
             include '../koneksi.php';
 
-            // Query untuk mengambil data penjualan aktual dari tabel dt_penjualan
-            $query = "SELECT db.merek AS merek_laptop, db.tipe AS tipe_laptop, 
-        tahun,
-        SUM(dp.dt_aktual) AS total_penjualan 
-        FROM dt_penjualan dp
-        JOIN dt_barang db ON dp.id_brg = db.id_brg
-        GROUP BY merek_laptop, tipe_laptop, tahun";
-            $result = mysqli_query($conn, $query);
+            $sql_merek = "SELECT DISTINCT merek FROM dt_barang";
+            $result = $conn->query($sql_merek);
 
-            // Data penjualan
-            $data_penjualan = [];
+            $jumlah_merek = 0;
 
-            // Loop melalui setiap baris hasil query dan menambahkannya ke array data penjualan
-            while ($row = mysqli_fetch_assoc($result)) {
-                $merek = $row['merek_laptop'];
-                $tipe = $row['tipe_laptop'];
-                $tahun = $row['tahun'];
-                $jumlah_penjualan = $row['total_penjualan'];
-
-                // Inisialisasi data penjualan untuk merek dan tipe tertentu jika belum ada
-                if (!isset($data_penjualan[$tahun][$merek][$tipe])) {
-                    $data_penjualan[$tahun][$merek][$tipe] = 0;
-                }
-
-                // Tambahkan jumlah penjualan ke data penjualan untuk merek, tipe, dan tahun tertentu
-                $data_penjualan[$tahun][$merek][$tipe] += $jumlah_penjualan;
-            }
-
-            // Buat array untuk merek laptop
-            $labels_merek = [];
-            // Buat array untuk tipe laptop
-            $labels_tipe = [];
-            foreach ($data_penjualan as $tahun_data) {
-                foreach ($tahun_data as $merek => $tipe_data) {
-                    if (!in_array($merek, $labels_merek)) {
-                        $labels_merek[] = $merek;
-                    }
-                    foreach ($tipe_data as $tipe => $jumlah) {
-                        if (!in_array($tipe, $labels_tipe)) {
-                            $labels_tipe[] = $tipe;
-                        }
-                    }
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $jumlah_merek++;
                 }
             }
 
-            // Buat array untuk tahun
-            $labels_tahun = array_keys($data_penjualan);
+            $sql_tipe = "SELECT DISTINCT tipe FROM dt_barang";
+            $result = $conn->query($sql_tipe);
 
-            // Data untuk datasets
-            $datasets = [];
+            $jumlah_tipe = 0;
 
-            // Loop melalui setiap tahun
-            foreach ($data_penjualan as $tahun => $tahun_data) {
-                // Inisialisasi array untuk menyimpan 3 merek dan tipe dengan jumlah penjualan tertinggi
-                $top_sales = [];
-
-                // Loop melalui data penjualan tahun ini
-                foreach ($tahun_data as $merek => $tipe_data) {
-                    foreach ($tipe_data as $tipe => $jumlah) {
-                        // Bandingkan jumlah penjualan dengan top 3
-                        if (count($top_sales) < 3) {
-                            $top_sales[] = ['merek' => $merek, 'tipe' => $tipe, 'jumlah' => $jumlah];
-                        } else {
-                            // Temukan dan ganti nilai terkecil di dalam top 3
-                            $min_index = 0;
-                            for ($i = 1; $i < count($top_sales); $i++) {
-                                if ($top_sales[$i]['jumlah'] < $top_sales[$min_index]['jumlah']) {
-                                    $min_index = $i;
-                                }
-                            }
-                            // Jika jumlah penjualan saat ini lebih besar dari nilai terkecil di dalam top 3, ganti nilainya
-                            if ($jumlah > $top_sales[$min_index]['jumlah']) {
-                                $top_sales[$min_index] = ['merek' => $merek, 'tipe' => $tipe, 'jumlah' => $jumlah];
-                            }
-                        }
-                    }
-                }
-
-                // Tambahkan data top sales ke dalam dataset
-                foreach ($top_sales as $top_sale) {
-                    $datasets[$top_sale['merek'] . ' ' . $top_sale['tipe']][] = $top_sale['jumlah'];
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $jumlah_tipe++;
                 }
             }
 
-            // Konversi data datasets menjadi format JSON
-            $datasets_json = json_encode(array_values($datasets));
+            $sql_penjualanTahun = "SELECT tahun, SUM(dt_aktual) AS total_dt_aktual FROM dt_penjualan GROUP BY tahun ORDER BY total_dt_aktual DESC LIMIT 1";
+            $result = $conn->query($sql_penjualanTahun);
+
+            $penjualan_tertinggi = 0;
+            $tahun_penjualan_tertinggi = '';
+
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $penjualan_tertinggi = $row["total_dt_aktual"]; 
+                $tahun_penjualan_tertinggi = $row["tahun"]; 
+            }
+
+            $conn->close();
             ?>
 
-            <!-- Container untuk line chart -->
-            <div class="chart-container">
-                <canvas id="myLineChart"></canvas>
+            <div class="gutter-container">
+                <div class="gutter row g-2">
+                    <div class="gutter col-4">
+                        <div class="gutter">
+                            <i class="gutter-icon bi bi-display"></i>
+                            <div class="gutter-item">
+                                <b>Jumlah Merek</b>
+                                <p><?php echo $jumlah_merek; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="gutter col-4">
+                        <div class="gutter">
+                            <i class="gutter-icon bi bi bi-laptop"></i>
+                            <div class="gutter-item">
+                                <b>Jumlah Tipe</b>
+                                <p><?php echo $jumlah_tipe; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="gutter col-4">
+                        <div class="gutter">
+                            <i class="gutter-icon bi bi-bar-chart"></i>
+                            <div class="gutter-item">
+                                <b>Penjualan Tertinggi</b>
+                                <p><?php echo "Tahun: " . $tahun_penjualan_tertinggi . " " . "Jumlah: " . $penjualan_tertinggi; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
 
-    <!-- Script untuk line chart -->
-    <script>
-        // Data untuk datasets
-        var datasets = <?php echo $datasets_json; ?>;
-
-        // Data untuk tahun
-        var tahun = <?php echo json_encode($labels_tahun); ?>;
-
-        // Konfigurasi line chart
-        var config = {
-            type: 'line',
-            data: {
-                labels: tahun,
-                datasets: [
-                    <?php foreach ($datasets as $label => $data) : ?> {
-                            label: '<?php echo $label; ?>',
-                            data: <?php echo json_encode($data); ?>,
-                            fill: false,
-                            pointStyle: 'circle',
-                        },
-                    <?php endforeach; ?>
-                ]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                },
-                plugins: {
-                    legend: {
-                        display: false
-                    }
-                }
-            }
-        };
-
-        // Inisialisasi line chart
-        var ctx = document.getElementById('myLineChart').getContext('2d');
-        var myLineChart = new Chart(ctx, config);
-    </script>
-
-    <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            <!-- Bootstrap -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
